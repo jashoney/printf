b0f421c12eca999d632b99172ca94a6497c8a967
jashoney <4479@holbertonschool.com>
2022-03-12 13:25:01 +1100

    fixed derefernecing issues

--- structarray.c
+++ structarray.c
@@ -11,26 +11,25 @@ int (*structarr(char id_type))(va_list)
 
 	int index;
 	func_struct mystruct[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"s", print_string},
-		{"d", print_int},
-		{"x", print_hex},
-		{"u", print_uns},
-		{"x", print_hex},
-		{"o", print_oct},
-		{"X", print_hex},
-		{"p", print_void},
+		{'c', print_char},
+		{'i', print_int},
+		{'s', print_string},
+		{'d', print_int},
+/*		{'x', print_hex},
+		{'u', print_uns},
+		{'o', print_oct},
+		{'X', print_hex},
+		{'p', print_void},*/
 		{'\0', NULL},
 	};
 
 	for (index = 0; mystruct[index].ch != '\0'; index++)
 	{
-		if (*(mystruct[index].ch) == id_type)
+		if (mystruct[index].ch == id_type)
 		{
 			break;
 		}
 
 	}
-		return (mystruct[index].f);
+	return (mystruct[index].f);
 }
