0a0acbb5ac0c1a211e34b8aef88b2358c0b82b44
jashoney <4479@holbertonschool.com>
2022-03-14 05:07:52 +1100

    betty _printf.c

--- _printf.c
+++ _printf.c
@@ -9,8 +9,9 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0, i = 0;				/* we return (count = strlen(format)) + loop variables */
-	int (*f)(va_list);				/* f is a ptr to a function that takes a va_list type as input and returns an int */
+	int count = 0, i = 0;				/* we return (count = strlen(output) */
+
+	int (*f)(va_list);				/* f is a ptr to a f() */
 	va_list args;					/* args is of type va_list */
 
 	if (format == NULL)				/* test format for NULL */
@@ -18,31 +19,32 @@ int _printf(const char *format, ...)
 	va_start(args, format);				/* start moving through vargs after format */
 	for (i = 0; format[i] != '\0'; i++)		/* move across format */
 	{
-		count++;				/* this saves lines in _printf, remove if next char is an identifier */
-		if (format[i] == '%')			/* if yes, check the next char in format to decdide what to do */
+		count++;				/* increment count, remove if next char is an identifier */
+		if (format[i] == '%')			/* if yes, check the next char in format */
 		{
-			if (format[i + 1] == '%' || format[i + 1] == '\0')  /* next char is % or end of string */
+			if (format[i + 1] == '%' ||
+				format[i + 1] == '\0')  /* next char is % or end of string */
 			{
 				_putchar(format[i]);		/* print the % */
 				if (format[i + 1] == '\0')	/* stop if at end of string */
 					return (count);
 				i++;				/* next char = %, but don't print it */
 			}
-			else 					/* check for identifier */
+		else						/* check for identifier using format[i + 1] */
 			{
 				f = structarr(format[i + 1]);   /* set f to a function or NULL */
 				if (f != NULL)			/* if f is not NULL it contains the function to use */
 				{
 					count = count - 1;		/* count now doesn't include the % */
-					count = count + f(args);	/* count increased by length of print by function call */
-					i++;				/* move to next char as we used i + 1 as well */
+					count = count + f(args);	/* length of print by f() call added */
+					i++;				/* move on to next char after identifier */
 				}
 				else
 					_putchar(format[i]);	/* identifier not found, print the % */
 			}
 		}
 		else
-			_putchar(format[i]);  		/* print current char in string format */
+			_putchar(format[i]);		/* print current char in string format */
 	}
 	va_end(args);					/* free up the va_list */
 	return (count);					/* _printf returns the total of the chars printed */
